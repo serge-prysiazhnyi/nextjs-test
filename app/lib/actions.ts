c7a9d3333921b3de95c1a0bfb84d56df6e73e4c4
Serhii Prysiazhnyi <serge.prysiazhnyi@gmail.com>
2024-11-28 14:24:56 +0100

    delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -53,3 +53,8 @@ export async function updateInvoice(id: string, formData: FormData) {
   expirePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  expirePath("/dashboard/invoices");
+}
